794305d8028dfe973da4446a3c08681832ab5a13
Anna <Anna@Laptop-von-Anna.eduroam.hs-furtwangen.de>
2018-10-05 11:02:40 +0200

    Abfrage für name = null

--- A00/A00.js
+++ A00/A00.js
@@ -3,6 +3,9 @@ var A00;
     document.addEventListener("DOMContentLoaded", greet);
     function greet() {
         let name = prompt("Wie heißt du?", "Dein Name");
+        if (name == "Dein Name") {
+            name = "misteriöser unbekannter";
+        }
         console.log("Hallo " + name + ", wie geht es dir?");
         document.getElementById("helloBox").innerHTML = "Hallo " + name + ", wie geht es dir?";
     }
--- A00/A00.ts
+++ A00/A00.ts
@@ -4,6 +4,10 @@ namespace A00 {
 
     function greet(): void {
         let name: string = prompt("Wie heißt du?", "Dein Name");
+        
+        if (name == "Dein Name") {
+            name = "misteriöser unbekannter";
+        }       
         console.log("Hallo " + name + ", wie geht es dir?");
         document.getElementById("helloBox").innerHTML = "Hallo " + name + ", wie geht es dir?";
     }
