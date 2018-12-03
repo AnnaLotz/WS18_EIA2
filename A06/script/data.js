903bb97ea6a3ef6e565191b9b0846af903114b26
AnnaLotz <anna.lotz@web.de>
2018-12-03 09:44:33 +0100

    created a6 folder

---
+++ A06/script/data.js
@@ -0,0 +1,35 @@
+var A6;
+(function (A6) {
+    A6.products = {
+        "trees": [
+            { name: "Nordmanntanne", price: 30 },
+            { name: "Fichte", price: 25 },
+            { name: "Blautanne", price: 30 }
+        ],
+        "stands": [
+            { name: "Runder Ständer Klassisch grün", price: 38 },
+            { name: "Metallständer silber", price: 46 }
+        ],
+        "baubles": [
+            { name: "Glaskugel Rot", price: 0.49 },
+            { name: "Glaskugel Gold", price: 0.99 },
+            { name: "Glaskugel Silber", price: 0.99 }
+        ],
+        "figures": [
+            { name: "Strohstern", price: 0.89 },
+            { name: "Holzfigur", price: 2.30 }
+        ],
+        "lights": [
+            { name: "Rot Echtwachs", price: 0.79 },
+            { name: "Weiß Echtwachs", price: 0.89 },
+            { name: "Weiß Elektrisch", price: 1.99 },
+            { name: "Lichterkette", price: 24.99 }
+        ],
+        "shipping": [
+            { name: "DHL", price: 5.99 },
+            { name: "Hermes", price: 5.99 },
+            { name: "Express", price: 8.99 }
+        ]
+    };
+})(A6 || (A6 = {})); //namespace zu
+//# sourceMappingURL=data.js.map
