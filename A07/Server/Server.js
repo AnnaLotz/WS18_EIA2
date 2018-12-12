061710fc01911e115015d96d21c157d9e04bb276
AnnaLotz <anna.lotz@web.de>
2018-12-12 09:24:32 +0100

    Server in standart eia2 ordner, nicht in node repo

---
+++ A07/Server/Server.js
@@ -0,0 +1,34 @@
+"use strict";
+const Http = require("http");
+const Url = require("url");
+var A06_SendData;
+(function (A06_SendData) {
+    console.log("Starting server");
+    let port = process.env.PORT;
+    if (port == undefined)
+        port = 8100;
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    function handleListen() {
+        console.log("Listening");
+    }
+    function handleRequest(_request, _response) {
+        if (_request.url != "/favicon.ico") {
+            console.log("I hear voices!");
+            _response.setHeader("content-type", "text/html; charset=utf-8");
+            _response.setHeader("Access-Control-Allow-Origin", "*");
+            let query = Url.parse(_request.url, true).query;
+            console.log("requestUrl: " + _request.url);
+            console.log(query);
+            for (let key in query) {
+                let product = query[key];
+                console.log(key + "= " + product.toString());
+                _response.write(key + " = " + product.toString() + "<br>");
+            }
+            _response.end();
+        }
+    } //close handleRequest
+})(A06_SendData || (A06_SendData = {})); //close Namespace
+//# sourceMappingURL=Server.js.map
---
+++ A07/Server/Server.ts
@@ -0,0 +1,48 @@
+import * as Http from "http";
+import * as Url from "url";
+
+namespace A06_SendData {
+
+    interface AssocProd {
+        [key: string]: string;
+    }
+
+    console.log("Starting server");
+    let port: number = process.env.PORT;
+    if (port == undefined)
+        port = 8100;
+
+    let server: Http.Server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+
+    function handleListen(): void {
+        console.log("Listening");
+    }
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        if (_request.url != "/favicon.ico") {
+
+            console.log("I hear voices!");
+
+            _response.setHeader("content-type", "text/html; charset=utf-8");
+            _response.setHeader("Access-Control-Allow-Origin", "*");
+
+            let query: AssocProd = Url.parse(_request.url, true).query;
+
+            console.log("requestUrl: " + _request.url);
+            console.log(query);
+
+            for (let key in query) {
+                let product: string = query[key];
+                console.log(key + "= " + product.toString());
+                _response.write(key + " = " + product.toString() + "<br>");
+            }
+
+
+            _response.end();
+        }
+    } //close handleRequest
+} //close Namespace
